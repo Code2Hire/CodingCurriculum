73167ca819696f07b9fbb77e0a2f62b09f2d5a83
Bobby Craig <bobbyrc28@gmail.com>
2019-06-23 22:42:06 -0400

    Added functionality to alpha.html

--- 01_Core/week02/toyProblems/js/scripts.js
+++ 01_Core/week02/toyProblems/js/scripts.js
@@ -1,3 +1,35 @@
 function alphaClick() {
-  console.log("Test")
+  event.preventDefault()
+
+  const userInput = document.getElementById("nameList").value
+  const TARGET_UL_ELEMENT = document.getElementById("sortedNameList")
+  let splitNames
+
+  splitNames = userInput.split(",")
+  splitNames = splitNames.map(currentElement => currentElement.trim())
+
+  let sortedNames = flipNames(splitNames)
+  sortedNames.sort()
+  sortedNames = flipNames(sortedNames)
+
+  console.log(userInput)
+  console.log(splitNames)
+  console.log(sortedNames)
+
+  TARGET_UL_ELEMENT.innerHTML = ""
+
+  sortedNames.forEach(currentName => {
+    const newListItem = document.createElement("li")
+    newListItem.innerText = currentName
+
+    TARGET_UL_ELEMENT.insertAdjacentElement("beforeend", newListItem)
+  })
+}
+
+const flipNames = function(nameArray) {
+  let flippedNameArray = nameArray.map(function(currentName) {
+    separatedName = currentName.split(" ")
+    return `${separatedName[1]} ${separatedName[0]}`
+  })
+  return flippedNameArray
 }

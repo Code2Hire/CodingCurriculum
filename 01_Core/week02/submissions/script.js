c7cbe1a25c49fee74179acfe2dcd22b768f83798
Chris <chrsmith@redventures.com>
2019-10-21 01:37:39 -0400

    Completed Week2 Toy Problem

--- 01_Core/week02/submissions/script.js
+++ 01_Core/week02/submissions/script.js
@@ -1,7 +1,32 @@
 var generateButton = document.querySelector("button");
 generateButton.addEventListener("click", generateList);
 
+function Person(firstName, lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+}
 
-function generateList(){
+function generateList(e){
+    e.preventDefault();
+    var nameList = [];
+    var list = document.querySelector(".name-list");
+    list.innerHTML = '';
 
+    var userInput = document.querySelector(".user-input").value;
+    
+    nameListArr = userInput.split(", ");
+
+    nameListArr.forEach(element => {
+        const newArr = element.split(" ");
+        const person = new Person(newArr[0], newArr[1]);
+        nameList.push(person);
+    });
+
+    nameList.sort((a, b) => (a.lastName > b.lastName) ? 1 : -1);
+
+    nameList.forEach(element => {
+        list.innerHTML += `<li>${element.firstName} <span class="red-word">${element.lastName}</span></li>`;
+    });
+    
+    document.querySelector(".js-fun-form").reset();
 }
